e40395e1e44080c2defd3734c70e4c7e3c09eaa0
serifcanozdeniz <ozdenizserifcan96@gmail.com>
2023-11-13 23:23:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let hr = document.querySelector('#hr');
+
+let mn = document.querySelector('#mn');
+
+let sc = document.querySelector('#sc');
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
+
+
+// digital clock
+
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
